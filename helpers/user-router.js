a7ac89961d724f6bc81e132d0f39291003bc0171
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-20 16:23:52 -0500

    get by id functioning

--- helpers/user-router.js
+++ helpers/user-router.js
@@ -18,15 +18,15 @@ router.get('/:id', (req, res) => {
     const {id} = req.params;
 
     db.getById(id)
-    .then(posts=> {
-        if (posts) {
-            res.status(201).json({success: true , posts});
+    .then(users=> {
+        if (users) {
+            res.status(201).json({success: true , users});
         } else {
-        res.status(404).json({ message: "The post with the specified ID does not exist."})
+        res.status(404).json({ message: "The user with the specified ID does not exist."})
         }
     })
     .catch(err => {
-        res.status(500).json({  error: "The post information could not be retrieved."})
+        res.status(500).json({  error: "The user information could not be retrieved."})
     })
 })
 
